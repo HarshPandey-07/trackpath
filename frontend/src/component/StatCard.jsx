7eb18cae99dab31c94f34de042e9c37931a0da71
Harsh <267274186+HarshPandey-07@users.noreply.github.com>
2026-09-12 13:10:43 +0530

    Stats & deadlines sections added to dashboard complete

---
+++ frontend/src/component/StatCard.jsx
@@ -0,0 +1,10 @@
+const StatCard = ({ title, value }) => {
+	return (
+		<div className="flex flex-col justify-around align-middle bg-(--cards) min-h-28 w-fit md:w-48 p-4 rounded-xl border border-(--border) shadow-(--shadow)">
+			<p>{title}</p>
+			<h1>{value}</h1>
+		</div>
+	);
+};
+
+export default StatCard;
